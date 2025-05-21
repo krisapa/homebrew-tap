f6e936ff03adf124756ad71c14a45b2bedcb99dc
Krish <57601958+krisapa@users.noreply.github.com>
2025-05-21 11:31:45 -0400

    Update macspresso.rb

--- Casks/macspresso.rb
+++ Casks/macspresso.rb
@@ -2,7 +2,7 @@ cask "macspresso" do
   version "1.0"
   sha256 "1be9309e9c2677f706374c749e152997f67f32bcd90718b4a922eebe6980e337"
 
-  url "https://github.com/6b70/MacSpresso/releases/download/#{version}/MacSpresso_#{version}.dmg"
+  url "https://github.com/krisapa/MacSpresso/releases/download/#{version}/MacSpresso_#{version}.dmg"
   name "MacSpresso"
   desc "Menu bar app to manage system caffeination"
   homepage "https://github.com/6b70/MacSpresso"
